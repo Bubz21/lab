4703134ecfbbf4e528ae0b5ff7cc8039d8e471fd
Bubz21 <104239968+Bubz21@users.noreply.github.com>
2022-04-22 21:30:30 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
 class Television:
+    """
+    A class that details channels and volumes for a tv
+    """
     MIN_CHANNEL = 0     # Minimum TV channel
     MAX_CHANNEL = 3     # Maximum TV channel
 
@@ -6,81 +9,81 @@ class Television:
     MAX_VOLUME = 2      # Maximum TV volume
 
     def __init__(self):
+        """
+        Constructor used to initial the state of the tv
+        """
         self.channel = 0
         self.volume = 0
         self.status = 'False'
-        """
-        - Create a private variable to store the TV channel. It should be set to the minimum TV channel by default.
-        - Create a private variable to store the TV volume. It should be set to the minimum TV volume by default.
-        - Create a private variable to store the TV status. The TV should start when it is off.
-        """
+        
         pass
 
     def power(self):
+        """
+        A method that allows the user to switch the power of the tv to on our off
+        returns the status of the tv
+        """
         if self.status == 'False':
             self.status = 'True'
 
         if self.status == 'True':
             self.status = 'False'
-        """
-        - This method should be used to turn the TV on/off.
-        - If called on a TV object that is off, the TV object should be turned on.
-        - If called on a TV object that is on, the TV object should be turned off.
-        """
+            
         pass
 
     def channel_up(self):
+        """
+        A method that modifies the channel up
+        returns the new channel that is chosen
+        """
         if self.status == 'True':
             if self.channel < 3:
                 self.channel = self.channel + 1
             else:
                 self.channel = 0
-        """
-        - This method should be used to adjust the TV channel by incrementing its value.
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MAX_CHANNEL, it should take the TV channel back to the MIN_CHANNEL.
-        """
+                
         pass
 
     def channel_down(self):
+        """
+        A method that modifies the channel down
+        returns the new channel that is chosen
+        """
         if self.status == 'True':
             if self.channel > 0:
                 self.channel = self.channel - 1
             else:
                 self.channel = 3
-        """
-        - This method should be used to adjust the TV channel by decrementing its value.
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MIN_CHANNEL, it should take the TV channel back to the MAX_CHANNEL.
-        """
+                
         pass
 
     def volume_up(self):
+        """
+        A method that modifies the volume up
+        returns the new channel that is chosen
+        """
         if self.status == 'True':
             if self.volume < 2:
                 self.volume = self.volume + 1
-        """
-        - This method should be used to adjust the TV volume by incrementing its value.
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MAX_VOLUME, the volume should not be adjusted.
-        """
+                
         pass
 
     def volume_down(self):
+        """
+        A method that modifies the volume down
+        returns the new channel that is chosen
+        """
         if self.status == 'True':
             if self.volume > 0:
                 self.volume = self.volume - 1
-        """
-        - This method should be used to adjust the TV volume by decrementing its value.
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MIN_VOLUME, the volume should not be adjusted.
-        """
+                
         pass
 
     def __str__(self):
+        """
+        A method that sends back the information of the status, channel, and volume
+        """
         self.send = ('TV status: Is on = ', self.status, ', Channel = ', self.channel, ', Volume = ', self.volume)
         return self.send
-        """
-        - This method should be used to return the TV status using the format shown in the comments of main.py
-        """
+    
         pass
