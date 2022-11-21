1662f3dfa8747371c038ebeb9b8948a82db76b28
Bubz21 <104239968+Bubz21@users.noreply.github.com>
2022-11-20 21:34:46 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,24 +1,50 @@
 class Account:
+  """
+  A class repressing details for an account
+  """
   def __init__(self,name):
-    self.account_name = name
-    self.account_balance = 0
+    """
+    Constructor to create initial state of an account object
+    :param name: Person's first name
+    :param balance: total amount of balance
+    """
+    self.__account_name = name
+    self.__account_balance = 0
   
   def deposit(self,amount):
+    """
+    Method to add to the account's balance
+    :return: True if the transaction is sucussesful
+    :return: False if the transaction is unsucussesful
+    """
     if amount > 0:
-      self.account_balance = self.account_balance + amount
+      self.__account_balance = self.__account_balance + amount
       return True
     else:
       return False
     
   def withdraw(self,amount)
+  """
+  Method to remove from the account's balance
+  :return: True if the transaction is sucussesful
+  :return: False if the transaction is unsucussesful
+  """
     if (amount > 0) and (amount < account_balance):
-      self.account_balance = self.account_balance - amount
+      self.__account_balance = self.__account_balance - amount
       return True
     else:
       return False
     
   def get_balance(self):
-    return self.account_balance
+    """
+    Method to access the account balance
+    :return: The account's balance
+    """
+    return self.__account_balance
     
   def get_name(self):
-    return self.account_name
+    """
+    Method to access the account name
+    :return: The account's name
+    """
+    return self.__account_name
